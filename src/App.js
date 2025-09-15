135fc88b6b648133bad62eb4060b86c069d374b3
Jhon Codev <132035349+jhoncodev@users.noreply.github.com>
2025-09-14 20:03:34 -0500

    Rutas actualizadas

--- src/App.js
+++ src/App.js
@@ -3,32 +3,47 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import MainLayout from './components/Layout/MainLayout';
 import Home from './pages/Home';
+
 import CitaMedica from './pages/admision/CitaMedica';
 import ProcedimientoAdmision from './pages/admision/ProcedimientoAdmision';
+
 import Agendadas from './pages/citas/Agendadas';
 import EnEspera from './pages/citas/EnEspera';
 import Finalizadas from './pages/citas/Finalizadas';
+
 import AgendaHoy from './pages/procedimientos/AgendaHoy';
 import Ordenes from './pages/procedimientos/Ordenes';
+import Agendados from './pages/procedimientos/Agendados';
+import Completados from './pages/procedimientos/Completados';
+import Altas from './pages/procedimientos/Altas';
+
 import NuevoPaciente from './pages/pacientes/NuevoPaciente';
 import BuscarPaciente from './pages/pacientes/BuscarPaciente';
-import Suministros from './pages/info-clinica/Suministros';
+
 import Examenes from './pages/info-clinica/Examenes';
+import Suministros from './pages/info-clinica/Suministros';
 import Cie10 from './pages/info-clinica/Cie10';
+
+import Detallado from './pages/estadisticas/Detallado';
+import Dashboard from './pages/estadisticas/Dashboard';
+import PacientesEstadisticas from './pages/estadisticas/Pacientes';
+import ProcedimientosEstadisticas from './pages/estadisticas/Procedimientos';
+import PersonalEstadisticas from './pages/estadisticas/Personal';
+
 import Centros from './pages/administracion/Centros';
-import Estudios from './pages/administracion/Estudios';
-import ExamenesAdmi from './pages/administracion/Examenes';
+import PersonalAdministracion from './pages/administracion/Personal';
 import Horario from './pages/administracion/Horario';
+import Estudios from './pages/administracion/Estudios';
+import Salas from './pages/administracion/Salas';
+import Recursos from './pages/administracion/Recursos';
+import ExamenesAdministracion from './pages/administracion/Examenes';
+import Preparacion from './pages/administracion/Preparacion';
+import Seguros from './pages/administracion/Seguros';
+import Roles from './pages/administracion/Roles';
+import Usuarios from './pages/administracion/Usuarios';
+import Plantillas from './pages/administracion/Plantillas';
 import Macros from './pages/administracion/Macros';
 import MedicosRef from './pages/administracion/MedicosRef';
-import Personal from './pages/administracion/Personal';
-import Plantillas from './pages/administracion/Plantillas';
-import Preparacion from './pages/administracion/Preparacion';
-import Recursos from './pages/administracion/Recursos';
-import Roles from './pages/administracion/Roles';
-import Salas from './pages/administracion/Salas';
-import Seguros from './pages/administracion/Seguros';
-import Usuarios from './pages/administracion/Usuarios';
 
 
 const theme = createTheme({
@@ -95,22 +110,35 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/admision/cita" element={<CitaMedica />} />
             <Route path="/admision/procedimiento" element={<ProcedimientoAdmision />} />
+
             <Route path="/citas/agendas" element={<Agendadas />} />
             <Route path="/citas/espera" element={<EnEspera />} />
             <Route path="/citas/finalizadas" element={<Finalizadas />} />
+           
             <Route path="/procedimientos/agenda-hoy" element={<AgendaHoy />} />
             <Route path="/procedimientos/ordenes" element={<Ordenes />} />
+            <Route path="/procedimientos/agendados" element={<Agendados />} />
+            <Route path="/procedimientos/completados" element={<Completados />} />
+            
             <Route path="/pacientes/nuevo-paciente" element={<NuevoPaciente />} />
             <Route path="/pacientes/buscar-paciente" element={<BuscarPaciente />} />
+            
             <Route path="/info-clinica/suministros" element={<Suministros />} />
             <Route path="/info-clinica/cie10" element={<Cie10 />} />
             <Route path="/info-clinica/examenes" element={<Examenes />} />
+           
+            <Route path="/estadisticas/detallado" element={<Detallado />} />
+            <Route path="/estadisticas/dashboard" element={<Dashboard />} />
+            <Route path="/estadisticas/pacientes" element={<PacientesEstadisticas />} />
+            <Route path="/estadisticas/procedimientos" element={<ProcedimientosEstadisticas />} />
+            <Route path="/estadisticas/personal" element={<PersonalEstadisticas />} />
+           
             <Route path="/administracion/centros" element={<Centros />} />
-            <Route path="/administracion/personal" element={<Personal />} />
+            <Route path="/administracion/personal" element={<PersonalAdministracion />} />
             <Route path="/administracion/horarios" element={<Horario />} />
             <Route path="/administracion/estudios" element={<Estudios />} />
             <Route path="/administracion/recursos" element={<Recursos />} />
-            <Route path="/administracion/examenes" element={<ExamenesAdmi />} />
+            <Route path="/administracion/examenes" element={<ExamenesAdministracion />} />
             <Route path="/administracion/salas" element={<Salas />} />
             <Route path="*" element={<Home />} />
           </Routes>
